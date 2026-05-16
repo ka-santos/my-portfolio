f85f8c8f569ed4d4e715365139c527d6a91f3cf8
ka-santos <k.carvalho@aluno.ifsp.edu.br>
2026-05-16 15:50:05 -0300

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="logo">Portfolio</div>
+
+      <nav>
+        <a href="">Início</a>
+        <a href="">Sobre</a>
+        <a href="">Projetos</a>
+        <a href="">Habilidades</a>
+        <a href="">Contato</a>
+      </nav>
+
+      <button className="themeButton">
+        ○
+      </button>
+    </header>
+  )
+}
---
+++ src/components/Hero.tsx
@@ -0,0 +1,46 @@
+import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
+
+export default function Hero() {
+  return (
+    <section className="hero">
+      <div className="heroText">
+        <span className="miniText">OLÁ, EU SOU</span>
+        
+
+        <h1>
+          Karen dos Santos<br />
+          Desenvolvedora Web <br />
+        </h1>
+
+        <p>
+          Criando experiências digitais únicas com foco em design elegante,
+          performance e acessibilidade.
+        </p>
+
+        <div className="heroButtons">
+          <button className="primaryButton">Ver Projetos</button>
+
+          <button className="secondaryButton">Entrar em Contato</button>
+        </div>
+
+        <div className="socials">
+          <span>Me encontre em:</span>
+
+          <div className="icons">
+            <FaGithub />
+            <FaLinkedin />
+            <FaEnvelope />
+          </div>
+        </div>
+      </div>
+
+      <div className="heroImage">
+        <div className="circle">
+          <div className="innerCircle">
+            <span>KS</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
